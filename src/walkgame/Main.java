04b2d0de18281d4d2ab8f5fe82edc9e6424dd0a4
tim laptop <timterwijn@quicknet.nl>
2018-03-28 10:07:11 +0200

    paar toto's er bij

--- src/walkgame/Main.java
+++ src/walkgame/Main.java
@@ -19,6 +19,14 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /*
+    *
+    * todo: Gameobject interface maken, alle javafx files inheriten in nieuwe classen met gameobject interface
+    * todo: objecten hebben stattic groups waar ze zelf in opslaan, die groups worden aan het begin al aan root/type gelinked, hierdoor geen interface meer nodig die type aangeeft
+    * todo: de map heeft 4 map propetys + een boolean die checkt of de player er voor het eerst is.
+    *
+    * */
+
     public static void main(String[] args) {
         launch(args);
     }
